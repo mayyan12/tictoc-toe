935a4c03a03ecd23116b7972aa7a254030af8aff
mayyan12 <katzmyyn@gmail.com>
2024-05-05 16:06:28 +0300

    bord

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 
 import InputBord from './componnts/inputBord/InputBord'
 import Button from './componnts/Button'
+import Loding from './componnts/Loding/Loding'
+import Bord from './componnts/bord/Bord'
 
 function App() {
 
 
   return (
-<>
-    <InputBord>
+    <>
+      <InputBord>
+        <Bord />
 
-    </InputBord>
-    
+      </InputBord>
+      {/* 
+      <Button />
+      <Loding
+        size="64px"
+        dotSize="8px"
+        dotCount={4}
+        color="#fcd015"
+        speed="1.1s"
+        spread="50deg"
+      /> */}
     </>
   )
 }
